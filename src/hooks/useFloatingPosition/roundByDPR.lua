7f5f596bccbf6af2f029891de63585e07edba5b9
Quamatic <lman71037@gmail.com>
2023-06-03 20:55:02 -0500

    More changes

--- src/hooks/useFloatingPosition/roundByDPR.lua
+++ src/hooks/useFloatingPosition/roundByDPR.lua
@@ -1,7 +1,12 @@
 local Workspace = game:GetService("Workspace")
 
+local function getViewportSize()
+	local camera = Workspace.CurrentCamera
+	return if camera == nil then Vector2.zero else camera.ViewportSize
+end
+
 local function roundByDPR(value: number)
-	local viewport = Workspace.CurrentCamera.ViewportSize
+	local viewport = getViewportSize()
 	local devicePixelRatio = viewport.X / viewport.Y
 
 	return math.round(value * devicePixelRatio) / devicePixelRatio
